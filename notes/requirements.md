15cc1053c090e8515a1e38c453a13fb2613bc0c6
Jordan Slater <32441504+JordanSlater@users.noreply.github.com>
2025-02-16 14:39:52 -0500

    Fix timer functionality

--- notes/requirements.md
+++ notes/requirements.md
@@ -1,4 +1,7 @@
 
 First I want to create the board through for loops
 
-
+- [x] Time completion time
+- [ ] Make splits
+    - [ ] Identify number of tiles in the correct position
+- [ ]
--- src/Timer.js
+++ src/Timer.js
@@ -16,15 +16,13 @@ export default function Timer( {startTime} ) {
   }, [isRunning, millisecondsSinceStart]);
   const id = "timer";
 
-  const hours   = Math.floor(millisecondsSinceStart / 3.6e6);
   const minutes = Math.floor((millisecondsSinceStart % 3.6e6) / 6e4);
   const seconds = Math.floor((millisecondsSinceStart % 6e4) / 1000);
   const milliseconds = Math.floor(millisecondsSinceStart % 1000);
 
-  const timeAsString = millisecondsSinceStart !== null ?
-      ((hours > 0 ? `${hours.toString().padStart(2, "0")}:` : "") +
-        `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}.${milliseconds.toString().padStart(3, "0")}`
-      )
+  const timeAsString =
+    millisecondsSinceStart !== null ?
+      (`${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}.${milliseconds.toString().padStart(3, "0")}`)
     :
       "00:00.000";
 
